0fca58aa0991ca2b777d28545fb8059dea3bbb69
Dobrivano <thatonepro12@gmail.com>
2026-03-03 19:31:40 +0200

    added gallery,  added back the contact form in about us html after it was deleted

--- tailwind-config.js
+++ tailwind-config.js
@@ -14,3 +14,24 @@ tailwind.config = {
     }
   }
 }
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        forest: '#0B2219', 
+        action: '#2DCC70',
+        sand: '#F4F1EA',
+        gold: '#C5A059'
+      },
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif'],
+      }
+    }
+  },
+  // Увери се, че новият файл е описан тук:
+  content: [
+    "./index.html",
+    "./about_us.html",
+    "./src/**/*.{html,js}"
+  ]
+}
